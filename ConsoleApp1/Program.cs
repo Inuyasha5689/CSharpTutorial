cdeebc439c1cc9f64baca5c3f1d4179069c40913
Drake Scott <dascott0204@gmail.com>
2017-04-01 21:29:32 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTut.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Args {0} : {1}", i, args[i]);
+            }
+
+            string[] myArgs = Environment.GetCommandLineArgs();
+
+            Console.WriteLine(string.Join(", ", myArgs));
+
+            SayHello();
+
+            Console.ReadLine();
+
+        }
+
+        private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What is your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0} : ", name);
+        }
+    }
+}
