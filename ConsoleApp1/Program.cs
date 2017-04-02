80b583aa39ff0c32610a6cc4fca0f237a74e411a
Drake Scott <dascott0204@gmail.com>
2017-04-01 22:39:38 -0500

    string built in functions in C# continued!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -23,14 +23,18 @@ namespace CSharpTut.cs
             //ushort : 16-big unsigned int - to 65,535
 
             string randString = "This is a string";
-            Console.WriteLine("String Length : {0}", randString.Length);
-            Console.WriteLine("String Contains is : {0}", randString.Contains("is"));
-            Console.WriteLine("Index of is : {0}", randString.IndexOf("is"));
-            Console.WriteLine("Remove String : {0}", randString.Remove(0, 6));
-            Console.WriteLine("Insert String : {0}", randString.Insert(10, "short "));
-            Console.WriteLine("Replace String : {0}", randString.Replace("string", "sentence"));
-            Console.WriteLine("Compare A to B : {0}", String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
 
+            Console.WriteLine("A = a : {0}", String.Equals("A", "a", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine("Pad Left : {0}", randString.PadLeft(20, '.'));
+            Console.WriteLine("Pad right : {0}", randString.PadRight(20, '.'));
+            Console.WriteLine("Trim : {0}", randString.Trim());
+            Console.WriteLine("Uppercase : {0}", randString.ToUpper());
+            Console.WriteLine("Lowercase : {0}", randString.ToLower());
+
+            string newString = String.Format("{0} saw a {1} {2} in the {3}", "Paul", "rabbit", "eating", "field");
+
+            Console.Write(@"Exactly What I Typed ' \");
 
 
             Console.ReadLine();
