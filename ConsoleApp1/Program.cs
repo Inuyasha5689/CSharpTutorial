3d92d7cff0a09c9e2ab0751051a377fc7a728b93
Drake Scott <dascott0204@gmail.com>
2017-04-01 22:32:25 -0500

    string built in functions in C#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,10 +22,15 @@ namespace CSharpTut.cs
             //ulong : 64-big unsigned int - to 18,446,744,073,709,551,615
             //ushort : 16-big unsigned int - to 65,535
 
-            Console.WriteLine("Currency : {0:c}", 23.455);
-            Console.WriteLine("Pad with 0s : {0:d4}", 23);
-            Console.WriteLine("3 Decimals : {0:f3}", 23.455);
-            Console.WriteLine("Commas : {0:n4}", 2300);
+            string randString = "This is a string";
+            Console.WriteLine("String Length : {0}", randString.Length);
+            Console.WriteLine("String Contains is : {0}", randString.Contains("is"));
+            Console.WriteLine("Index of is : {0}", randString.IndexOf("is"));
+            Console.WriteLine("Remove String : {0}", randString.Remove(0, 6));
+            Console.WriteLine("Insert String : {0}", randString.Insert(10, "short "));
+            Console.WriteLine("Replace String : {0}", randString.Replace("string", "sentence"));
+            Console.WriteLine("Compare A to B : {0}", String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
+
 
 
             Console.ReadLine();
